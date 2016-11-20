e63f03d7c4a34c742623a1022cd819292385ed7f
Afzal <afzalmlakdawala@gmail.com>
2016-11-20 19:01:59 +0530

    dashboard sorting done

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_admin_user!
   def index
     @no_of_late_users = Timelog.nos_of_late_users_today
-    @timelogs = Timelog.paginate(:page => params[:page], :per_page => 10)
+    @timelogs = Timelog.joins(:user).select("*, users.name as username").paginate(:page => params[:page], :per_page => 10).order("users.name asc, login_time desc")
   end
 end
